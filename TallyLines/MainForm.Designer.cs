69ae9d19582c6cc97d329374f392a456e32a4da4
Victor <publicdomainvic@gmail.com>
2023-04-02 16:31:30 -0400

    Add project files

---
+++ TallyLines/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace TallyLines
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textFileOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.minimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.customizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.setFirstCharacters20ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.freeReleasesPublicDomainisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.originalThreadDonationCodercomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sourceCodeGithubcomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+			this.itemsTextToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.itemsToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.processTallyButton = new System.Windows.Forms.Button();
+			this.displayLabel = new System.Windows.Forms.Label();
+			this.displayTextBox = new System.Windows.Forms.TextBox();
+			this.mainMenuStrip.SuspendLayout();
+			this.mainStatusStrip.SuspendLayout();
+			this.mainTableLayoutPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textFileOpenFileDialog
+			// 
+			this.textFileOpenFileDialog.DefaultExt = "txt";
+			this.textFileOpenFileDialog.Filter = "TXT Files|*.txt|All files (*.*)|*.*";
+			this.textFileOpenFileDialog.Multiselect = true;
+			// 
+			// mainMenuStrip
+			// 
+			this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.fileToolStripMenuItem1,
+									this.minimizeToolStripMenuItem,
+									this.toolsToolStripMenuItem,
+									this.helpToolStripMenuItem});
+			this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+			this.mainMenuStrip.Name = "mainMenuStrip";
+			this.mainMenuStrip.Size = new System.Drawing.Size(434, 24);
+			this.mainMenuStrip.TabIndex = 31;
+			// 
+			// fileToolStripMenuItem1
+			// 
+			this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.newToolStripMenuItem,
+									this.openToolStripMenuItem,
+									this.toolStripSeparator3,
+									this.exitToolStripMenuItem1});
+			this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+			this.fileToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem1.Text = "&File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripMenuItem.Image")));
+			this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.newToolStripMenuItem.Text = "&New";
+			this.newToolStripMenuItem.Click += new System.EventHandler(this.OnNewToolStripMenuItemClick);
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+			this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem.Text = "&Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.OnOpenToolStripMenuItemClick);
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(149, 6);
+			// 
+			// exitToolStripMenuItem1
+			// 
+			this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+			this.exitToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem1.Text = "E&xit";
+			this.exitToolStripMenuItem1.Click += new System.EventHandler(this.OnExitToolStripMenuItem1Click);
+			// 
+			// minimizeToolStripMenuItem
+			// 
+			this.minimizeToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+			this.minimizeToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+			this.minimizeToolStripMenuItem.Visible = false;
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.customizeToolStripMenuItem,
+									this.optionsToolStripMenuItem});
+			this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+			this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.toolsToolStripMenuItem.Text = "&Tools";
+			// 
+			// customizeToolStripMenuItem
+			// 
+			this.customizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.setFirstCharacters20ToolStripMenuItem});
+			this.customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
+			this.customizeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.customizeToolStripMenuItem.Text = "&Customize";
+			// 
+			// setFirstCharacters20ToolStripMenuItem
+			// 
+			this.setFirstCharacters20ToolStripMenuItem.Name = "setFirstCharacters20ToolStripMenuItem";
+			this.setFirstCharacters20ToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+			this.setFirstCharacters20ToolStripMenuItem.Text = "&Set first characters (20)";
+			this.setFirstCharacters20ToolStripMenuItem.Click += new System.EventHandler(this.OnSetFirstCharacters20ToolStripMenuItemClick);
+			// 
+			// optionsToolStripMenuItem
+			// 
+			this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.alwaysOnTopToolStripMenuItem});
+			this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+			this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.optionsToolStripMenuItem.Text = "&Options";
+			this.optionsToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.OnOptionsToolStripMenuItemDropDownItemClicked);
+			// 
+			// alwaysOnTopToolStripMenuItem
+			// 
+			this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+			this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+			this.alwaysOnTopToolStripMenuItem.Text = "&Always on top";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.freeReleasesPublicDomainisToolStripMenuItem,
+									this.originalThreadDonationCodercomToolStripMenuItem,
+									this.sourceCodeGithubcomToolStripMenuItem,
+									this.toolStripSeparator2,
+									this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// freeReleasesPublicDomainisToolStripMenuItem
+			// 
+			this.freeReleasesPublicDomainisToolStripMenuItem.Name = "freeReleasesPublicDomainisToolStripMenuItem";
+			this.freeReleasesPublicDomainisToolStripMenuItem.Size = new System.Drawing.Size(278, 22);
+			this.freeReleasesPublicDomainisToolStripMenuItem.Text = "Free Releases @ PublicDomain.is";
+			this.freeReleasesPublicDomainisToolStripMenuItem.Click += new System.EventHandler(this.OnFreeReleasesPublicDomainisToolStripMenuItemClick);
+			// 
+			// originalThreadDonationCodercomToolStripMenuItem
+			// 
+			this.originalThreadDonationCodercomToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("originalThreadDonationCodercomToolStripMenuItem.Image")));
+			this.originalThreadDonationCodercomToolStripMenuItem.Name = "originalThreadDonationCodercomToolStripMenuItem";
+			this.originalThreadDonationCodercomToolStripMenuItem.Size = new System.Drawing.Size(278, 22);
+			this.originalThreadDonationCodercomToolStripMenuItem.Text = "&Original thread @ DonationCoder.com";
+			this.originalThreadDonationCodercomToolStripMenuItem.Click += new System.EventHandler(this.OnOriginalThreadDonationCodercomToolStripMenuItemClick);
+			// 
+			// sourceCodeGithubcomToolStripMenuItem
+			// 
+			this.sourceCodeGithubcomToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("sourceCodeGithubcomToolStripMenuItem.Image")));
+			this.sourceCodeGithubcomToolStripMenuItem.Name = "sourceCodeGithubcomToolStripMenuItem";
+			this.sourceCodeGithubcomToolStripMenuItem.Size = new System.Drawing.Size(278, 22);
+			this.sourceCodeGithubcomToolStripMenuItem.Text = "Source code @ Github.com";
+			this.sourceCodeGithubcomToolStripMenuItem.Click += new System.EventHandler(this.OnSourceCodeGithubcomToolStripMenuItemClick);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(275, 6);
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(278, 22);
+			this.aboutToolStripMenuItem.Text = "&About...";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.OnAboutToolStripMenuItemClick);
+			// 
+			// mainStatusStrip
+			// 
+			this.mainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.itemsTextToolStripStatusLabel,
+									this.itemsToolStripStatusLabel});
+			this.mainStatusStrip.Location = new System.Drawing.Point(0, 440);
+			this.mainStatusStrip.Name = "mainStatusStrip";
+			this.mainStatusStrip.Size = new System.Drawing.Size(434, 22);
+			this.mainStatusStrip.SizingGrip = false;
+			this.mainStatusStrip.TabIndex = 30;
+			// 
+			// itemsTextToolStripStatusLabel
+			// 
+			this.itemsTextToolStripStatusLabel.Name = "itemsTextToolStripStatusLabel";
+			this.itemsTextToolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+			this.itemsTextToolStripStatusLabel.Text = "Items:";
+			// 
+			// itemsToolStripStatusLabel
+			// 
+			this.itemsToolStripStatusLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.itemsToolStripStatusLabel.Name = "itemsToolStripStatusLabel";
+			this.itemsToolStripStatusLabel.Size = new System.Drawing.Size(14, 17);
+			this.itemsToolStripStatusLabel.Text = "0";
+			// 
+			// mainTableLayoutPanel
+			// 
+			this.mainTableLayoutPanel.ColumnCount = 1;
+			this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.mainTableLayoutPanel.Controls.Add(this.processTallyButton, 0, 2);
+			this.mainTableLayoutPanel.Controls.Add(this.displayLabel, 0, 0);
+			this.mainTableLayoutPanel.Controls.Add(this.displayTextBox, 0, 1);
+			this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 24);
+			this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+			this.mainTableLayoutPanel.RowCount = 3;
+			this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+			this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+			this.mainTableLayoutPanel.Size = new System.Drawing.Size(434, 416);
+			this.mainTableLayoutPanel.TabIndex = 32;
+			// 
+			// processTallyButton
+			// 
+			this.processTallyButton.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.processTallyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+			this.processTallyButton.Location = new System.Drawing.Point(3, 374);
+			this.processTallyButton.Name = "processTallyButton";
+			this.processTallyButton.Size = new System.Drawing.Size(428, 39);
+			this.processTallyButton.TabIndex = 0;
+			this.processTallyButton.Text = "&Process tally";
+			this.processTallyButton.UseVisualStyleBackColor = true;
+			this.processTallyButton.Click += new System.EventHandler(this.OnProcessTallyButtonClick);
+			// 
+			// displayLabel
+			// 
+			this.displayLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.displayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.displayLabel.Location = new System.Drawing.Point(3, 0);
+			this.displayLabel.Name = "displayLabel";
+			this.displayLabel.Size = new System.Drawing.Size(428, 25);
+			this.displayLabel.TabIndex = 1;
+			this.displayLabel.Text = "&Display:";
+			this.displayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// displayTextBox
+			// 
+			this.displayTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.displayTextBox.Location = new System.Drawing.Point(3, 28);
+			this.displayTextBox.Multiline = true;
+			this.displayTextBox.Name = "displayTextBox";
+			this.displayTextBox.Size = new System.Drawing.Size(428, 340);
+			this.displayTextBox.TabIndex = 2;
+			this.displayTextBox.TextChanged += new System.EventHandler(this.OnDisplayTextBoxTextChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(434, 462);
+			this.Controls.Add(this.mainTableLayoutPanel);
+			this.Controls.Add(this.mainMenuStrip);
+			this.Controls.Add(this.mainStatusStrip);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "TallyLines";
+			this.mainMenuStrip.ResumeLayout(false);
+			this.mainMenuStrip.PerformLayout();
+			this.mainStatusStrip.ResumeLayout(false);
+			this.mainStatusStrip.PerformLayout();
+			this.mainTableLayoutPanel.ResumeLayout(false);
+			this.mainTableLayoutPanel.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox displayTextBox;
+		private System.Windows.Forms.Label displayLabel;
+		private System.Windows.Forms.Button processTallyButton;
+		private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+		private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem setFirstCharacters20ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem customizeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripStatusLabel itemsToolStripStatusLabel;
+		private System.Windows.Forms.ToolStripStatusLabel itemsTextToolStripStatusLabel;
+		private System.Windows.Forms.StatusStrip mainStatusStrip;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem sourceCodeGithubcomToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem originalThreadDonationCodercomToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem freeReleasesPublicDomainisToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem minimizeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+		private System.Windows.Forms.MenuStrip mainMenuStrip;
+		private System.Windows.Forms.OpenFileDialog textFileOpenFileDialog;
+	}
+}
